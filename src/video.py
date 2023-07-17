1323619ba162d95edd87ee37c3dc5a752aa0e645
Victor Volkov <vektorn1212@gmail.com>
2023-07-17 22:13:22 +0200

    hotfix: fix video.py

--- src/video.py
+++ src/video.py
@@ -1,9 +1,13 @@
-import json
 import os
+
 from googleapiclient.discovery import build
 
 
 class Video:
+    """
+    Класс для ютуб-канала, который получает информацию о видео по его id
+    """
+
     api_key: str = os.getenv('YT_API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
 
@@ -17,21 +21,21 @@ class Video:
         self.views_count: str = self.video_response['items'][0]['statistics']['viewCount']
         self.likes_count: int = self.video_response['items'][0]['statistics']['likeCount']
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        print(json.dumps(self.video_response, indent=2, ensure_ascii=False))
-
     def __str__(self):
+        """
+        Выводит название канала для пользователей
+        """
         return self.video_title
 
 
 class PLVideo(Video):
-
+    """
+    Дочерний класс(Video) для ютуб-канала, который получает информацию плейлисте по его id, а также всю информацию
+    родительского класса
+    """
     def __init__(self, video_id: str, playlist_id):
         super().__init__(video_id)
         self.playlist_id: str = playlist_id
-        self.playlist_videos = Video.youtube.playlistItems().list(playlistId=playlist_id,
-                                                                  part='contentDetails',
+        self.playlist_videos = Video.youtube.playlistItems().list(playlistId=playlist_id, part='contentDetails',
                                                                   maxResults=50,
                                                                   ).execute()
-
